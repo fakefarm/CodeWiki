dcd701fbba1917feb3ee62876d63205ca5019a64
Dave Woodall <dmwoodall@gmail.com>
2012-11-26 06:52:49 -0700

    rails new without test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodeWikiApp::Application.config.secret_token = 'e8c93765fb457ed5e5626de080caa845f28701caacd856301753e01a3270af44a85ffd938c1040ffbc6204896108ea1984899e499e015e0bc00904fbf35c0bdc'
